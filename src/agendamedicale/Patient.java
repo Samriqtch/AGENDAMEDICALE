fea44ef289d52fa492639165d04335fd496de420
Samson KLOUGAN <99410105+Samriqtch@users.noreply.github.com>
2025-04-27 16:34:20 +0100

    Initialisation des attributs avec le constructeur Méthode getters pour la accéder au attributs Méthode setters pour la mise à jour

--- src/agendamedicale/Patient.java
+++ src/agendamedicale/Patient.java
@@ -8,6 +8,77 @@ package agendamedicale;
  *
  * @author kloug
  */
+import java.time.LocalDate;
 public class Patient {
+    
+        private String nom;
+    private String prenom;
+    private String telephone;
+    private LocalDate datenaissance;
+    private String email;
+    private String adresse;
+    private String sexe;
+    
+    // contructeur 
+    public  patient(String nom, String prenom,String telephone,String email,String adresse,LocalDate datenaissance, String sexe) {
+        //initialisation des attributs 
+        this.nom=nom;
+        this.prenom=prenom;
+        this.telephone=telephone;
+        this.adresse=adresse;
+        this.email=email;
+        this.datenaissance=datenaissance;
+        this.sexe=sexe;
+        
+    }
+    //Méthode getters pour récuperer les valeurs des attributs 
+    public String getnom(){
+      return nom;  
+    }
+    
+    public String getprenom(){
+        return prenom;
+    }
+    public String gettelephone(){
+        return telephone;
+    }
+    public String getemail(){
+        return email;
+    }
+    public String getadresse(){
+        return adresse;
+    }
+    public LocalDate getdatenaissance(){
+        return datenaissance;
+    }
+    public String getsexe(){
+        return sexe;
+    }
+    
+    //Méthode setters pou accéder au valeurs des attributs et faire une mise à jour
+    public void  setprenom(){
+        this.prenom= prenom;
+    }
+    
+    public void setnom(){
+        this.nom=nom;
+    }
+    public void setadresse(){
+        this.adresse=adresse;
+    }
+    
+    public void settelephone(){
+        this.telephone=telephone;
+    }
+    public void setemail(){
+        this.email=email;
+    }
+    
+    public void setdatenaissance(){
+        this.datenaissance=datenaissance;
+    }
+    public void setsexe(){
+        this.sexe=sexe;
+    }
     
 }
