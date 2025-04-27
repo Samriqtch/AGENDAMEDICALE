f5d0a15c8b5b8b326177da37b2b7b7f3a1d3ff4b
Samson KLOUGAN <99410105+Samriqtch@users.noreply.github.com>
2025-04-27 16:01:28 +0100

    Constructeur de la classe patient, initialisation Méthode getters pour accéder au attributs de la classe Méthode setters pour la mise à jour des valeurs des attributs

--- src/agendamedicale/patient.java
+++ src/agendamedicale/patient.java
@@ -8,6 +8,73 @@ package agendamedicale;
  *
  * @author kloug
  */
+import java.time.LocalDate;
+
 public class patient {
+    
+    private String nom;
+    private String prenom;
+    private String telephone;
+    private LocalDate datenaissance;
+    private String email;
+    private String adresse;
+    
+    // contructeur 
+    public  patient(String nom, String prenom,String telephone,String email,String adresse,LocalDate datenaissance) {
+        //initialisation des attributs 
+        this.nom=nom;
+        this.prenom=prenom;
+        this.telephone=telephone;
+        this.adresse=adresse;
+        this.email=email;
+        this.datenaissance=datenaissance;
+        
+    }
+    //Méthode getters pour récuperer les valeurs des attributs 
+    public String getnom(){
+      return nom;  
+    }
+    
+    public String getprenom(){
+        return prenom;
+    }
+    public String gettelephone(){
+        return telephone;
+    }
+    public String getemail(){
+        return email;
+    }
+    public String getadresse(){
+        return adresse;
+    }
+    public LocalDate getdatenaissance(){
+        return datenaissance;
+    }
+    
+    //Méthode setters pou accéder au valeurs des attributs et faire une mise à jour
+    public void  setprenom(){
+        this.prenom= prenom;
+    }
+    
+    public void setnom(){
+        this.nom=nom;
+    }
+    public void setadresse(){
+        this.adresse=adresse;
+    }
+    
+    public void settelephone(){
+        this.telephone=telephone;
+    }
+    public void setemail(){
+        this.email=email;
+    }
+    
+    public void setdatenaissance(){
+        this.datenaissance=datenaissance;
+    }
+    
+    
+    
     
 }
