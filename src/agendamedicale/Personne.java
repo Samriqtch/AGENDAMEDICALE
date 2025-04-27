75c3e5b5f3d5b98886447206d7304da06c12fec4
Samson KLOUGAN <99410105+Samriqtch@users.noreply.github.com>
2025-04-27 20:37:05 +0100

    Version 1

--- src/agendamedicale/Personne.java
+++ src/agendamedicale/Personne.java
@@ -8,6 +8,68 @@ package agendamedicale;
  *
  * @author kloug
  */
+//Declaration de classe Mère 
 public class Personne {
     
+    private String nom;
+    private String prenom;
+    private String telephone;
+    private String email;
+    private String adresse;
+    private String sexe;
+    
+    // contructeur 
+    public Personne   (String nom, String prenom,String telephone,String email,String adresse, String sexe) {
+        //initialisation des attributs 
+        this.nom=nom;
+        this.prenom=prenom;
+        this.telephone=telephone;
+        this.adresse=adresse;
+        this.email=email;
+        this.sexe=sexe;
+        
+    }
+    //Méthode getters pour récuperer les valeurs des attributs 
+    public String getnom(){
+      return nom;  
+    }
+    
+    public String getprenom(){
+        return prenom;
+    }
+    public String gettelephone(){
+        return telephone;
+    }
+    public String getemail(){
+        return email;
+    }
+    public String getadresse(){
+        return adresse;
+    }
+    public String getsexe(){
+        return sexe;
+    }
+    
+    //Méthode setters pou accéder au valeurs des attributs et faire une mise à jour
+    public void  setprenom(){
+        this.prenom= prenom;
+    }
+    
+    public void setnom(){
+        this.nom=nom;
+    }
+    public void setadresse(){
+        this.adresse=adresse;
+    }
+    
+    public void settelephone(){
+        this.telephone=telephone;
+    }
+    public void setemail(){
+        this.email=email;
+    }
+    
+    public void setsexe(){
+        this.sexe=sexe;
+    }
 }
--- src/agendamedicale/priseRdv.java
+++ src/agendamedicale/priseRdv.java
@@ -8,6 +8,26 @@ package agendamedicale;
  *
  * @author kloug
  */
+import java.time.LocalDateTime;
+
 public class priseRdv {
-    
+    private Patient patient;
+    private Medecin medecin;
+    private LocalDateTime dateHeure;
+
+    public priseRdv(Patient patient, Medecin medecin, LocalDateTime dateHeure) {
+        this.patient = patient;
+        this.medecin = medecin;
+        this.dateHeure = dateHeure;
+    }
+
+    public Patient getPatient() { return patient; }
+    public Medecin getMedecin() { return medecin; }
+    public LocalDateTime getDateHeure() { return dateHeure; }
+
+    @Override
+    public String toString() {
+        return "RDV de " + patient + " avec " + medecin + " à " + dateHeure;
+    }
 }
+
